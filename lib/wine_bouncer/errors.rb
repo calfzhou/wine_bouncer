5da805d62f129cf60179169fb81e8cf2dbd59a72
Ji Zhou <calf.zhou@gmail.com>
2015-08-09 17:48:27 +0800

    bind doorkeeper error response into WineBouncer errors

--- lib/wine_bouncer/errors.rb
+++ lib/wine_bouncer/errors.rb
@@ -1,7 +1,19 @@
 module WineBouncer
   module Errors
     class UnconfiguredError < StandardError; end
-    class OAuthUnauthorizedError < StandardError; end
-    class OAuthForbiddenError < StandardError; end
+
+    class OAuthUnauthorizedError < StandardError
+      attr_reader :response
+      def initialize(response)
+        @response = response
+      end
+    end
+
+    class OAuthForbiddenError < StandardError
+      attr_reader :response
+      def initialize(response)
+        @response = response
+      end
+    end
   end
 end
